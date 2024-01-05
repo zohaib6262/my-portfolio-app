a198e2493e47c2eb0832d4c4a90365016d94bfea
zohaib6262 <zohaibbinarhraaf@gmail.com>
2024-01-05 09:05:24 -0800

    Navbar changes

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -35,6 +35,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import styles from "./NavBar.module.css";
 
 function NavBar() {
   return (
